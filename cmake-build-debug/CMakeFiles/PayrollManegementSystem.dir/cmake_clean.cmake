a38918a793ed7eebe7dad90446fb07c2e32bc7b8
yusuf_sargin <yusuf.sargin@sebit.com.tr>
2020-12-08 22:34:24 +0300

    initproject

---
+++ cmake-build-debug/CMakeFiles/PayrollManegementSystem.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/PayrollManegementSystem.dir/main.cpp.obj"
+  "PayrollManegementSystem.exe"
+  "PayrollManegementSystem.exe.manifest"
+  "PayrollManegementSystem.lib"
+  "PayrollManegementSystem.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/PayrollManegementSystem.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
